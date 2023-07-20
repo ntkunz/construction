34fdf57ba8b6a136092a96de56f118761e8870e6
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-07-20 06:06:24 -0700

    adjust sizing of text and links to prevent wrapping on mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,11 +3,11 @@ import "./Header.scss";
 export default function Header() {
 	return (
 		<header className="header">
-			<h2 className="header__text">Nicholas Kunz</h2>
+			<h1 className="header__text">Nicholas Kunz</h1>
 			<h2 className="header__text">Web Developer</h2>
-			<h1 className="header__text construction">Portfolio</h1>
-			<h1 className="header__text construction">Under</h1>
-			<h1 className="header__text construction">Construction</h1>
+			<h3 className="header__text construction">Portfolio</h3>
+			<h3 className="header__text construction">Under</h3>
+			<h3 className="header__text construction">Construction</h3>
 		</header>
 	);
 }
