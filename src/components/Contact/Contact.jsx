c82a9daac38f0e9723982825ba97c8867b82cb13
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-08-23 08:47:35 -0700

    Fix link to helping-neighbors. Add rel attribure links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,31 +2,40 @@ import "./Contact.scss";
 
 export default function Contact() {
 	return (
-		<nav className="contact">
-			<a href="https://github.com/ntkunz/" className="contact__link" target="_blank">
+		<nav className='contact'>
+			<a
+				href='https://github.com/ntkunz/'
+				className='contact__link'
+				target='_blank'
+				rel='noreferrer'>
 				Github
 			</a>
 
-			<a href="https://www.linkedin.com/in/nich-kunz/" className="contact__link" target="_blank">
+			<a
+				href='https://www.linkedin.com/in/nich-kunz/'
+				className='contact__link'
+				target='_blank'
+				rel='noreferrer'>
 				LinkedIn
 			</a>
 
-			<a href="./Nicholas_Kunz_Resume.pdf" className="contact__link" download>
+			<a href='./Nicholas_Kunz_Resume.pdf' className='contact__link' download>
 				Resume
 			</a>
 
-			<a href="https://www.helping-neighbors.nicholaskunz.com" className="contact__link">
+			<a
+				href='https://helping-neighbors.nicholaskunz.com'
+				className='contact__link'
+				target='_blank'
+				rel='noreferrer'>
 				Current Project
 			</a>
 
 			<a
-				href="mailto:mrnicholaskunz@gmail.com?subject=Connecting from NicholasKunz.com"
-				className="contact__link"
-			>
+				href='mailto:mrnicholaskunz@gmail.com?subject=Connecting from NicholasKunz.com'
+				className='contact__link'>
 				Email
 			</a>
-
-
 		</nav>
 	);
 }
