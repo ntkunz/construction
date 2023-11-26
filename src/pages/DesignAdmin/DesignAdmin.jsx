311b6cff88468f65b4e13ccd8d00eefbf024823d
ntkunz <33107555+ntkunz@users.noreply.github.com>
2023-11-26 08:25:25 -0800

    add images and designExample array

--- src/pages/DesignAdmin/DesignAdmin.jsx
+++ src/pages/DesignAdmin/DesignAdmin.jsx
@@ -1,11 +1,65 @@
 import "./DesignAdmin.scss";
-import Contact from "../../components/Contact/Contact";
+// import Contact from "../../components/Contact/Contact";
 
 export default function DesignAdmin() {
+
+
+   const designExamples = [
+      {
+         title: 'Blood Wizard x Adidas Colab Shoe',
+         role: 'Assisted layout, print preparation, and video production',
+         images: ['/design_images/bw_x_adidas_shoe_layout.jpg'],
+         description: `An exciting time in a smaller brand's life, The team and I at Blood Wizard Skateboards were able to release a shoe with Adidas Originals`,
+         link: {
+            linkText: 'Watch promotional video',
+            linkUrl: 'https://www.youtube.com/watch?v=9jEGLPJ2JR4'
+         },
+      },
+      {
+         title: 'Blood Wizard Board Catalog',
+         role: 'Vectorized and colored graphics, print preparation, and card layout',
+         images: ['/design_images/bw_catalog_2014.jpg'],
+         description: 'Skateboarding is great'
+      },
+      {
+         title: 'Helping Neighbors',
+         role: 'Web Design and Development',
+         images: [],
+         description: 'A recent personal project to learn web development and design',
+         link: {
+            linkText: 'Visit website',
+            linkUrl: 'https://helping-neighbors.nicholaskunz.com',
+         },
+      },
+      {
+         title: 'Aperture Rings for Camera Shutters',
+         role: `Sourced and produced laser cut aperture rings for frankensteined camera lenses and shutters`,
+         images: ['/design_images/calipers_and_aperture_ring.jpg', '/design_images/seiko_masking_tape_calibrate.jpg', '/design_images/straight_aperture_ring.png',
+            '/design_images/curved_aperture_ring.png', '/design_images/aperture_ring_1.jpg', '/design_images/aperture_ring_2.jpg', '/design_images/aperture_ring_3.jpg'],
+         description: 'When I stepped into my role at RayKo Photography Center, the proprietor had been collecting mis-matched lenses to shutters for years but had yet to find a way to produce the necessary aperture scales to sync the two. I was tasked with this and after some iterations and researching, I found an engraver able to take my Illustrator files and create perfect scales for the shutters. This transformed many many door-stops into sellable lens/shutter combinations',
+      },
+      {
+         title: 'Book and Zine layouts',
+         role: 'Layout and production',
+         images: ['/design_images/reflections_screenthot.png', '/design_images/photography_book_spread.png', '/design_images/japan_book_spread.png', '/design_images/gift_horse_book_turning_page.jpg'],
+         description: 'Some different personal and group zine and book projects from over the years'
+      },
+      {
+         title: 'Forest Service Banner',
+         role: 'Banner design',
+         images: ['public/design_images/fsbanner.png'],
+         description: 'My early steps into digital production, I had the chance to create a website for the US Forest Service. Here is a piece that has survived since that time many moons ago'
+      }
+   ]
+
+
    return (
       <main className="design-admin">
          <h1>Good day design office!</h1>
-         <h2>I'd like to put my hat in the ring for the part-time office administrator position<br /><br />- Nicholas</h2>
+         <h2>I am applying to your administration position</h2>
+         <p>I've added here some design examples from my past. I understand that the position is for admin duties, not design work. I didn't feel it would hurt to show that I can assist in multiple ways if ever needed</p>
+
+         <p>Thank you for your consideration and I look forward to hearing from you</p>
 
          <p>See resume and cover letter for contact information</p>
 
@@ -40,23 +94,27 @@ export default function DesignAdmin() {
 
          <aside className="excuse-me">Please excuse the quick pdf page formatting. Work in progress...</aside>
 
+         <section className="design-examples">
+            <h2>Design Examples</h2>
+            {designExamples.map((item, index) => (
+               <div key={index} className="design-item">
+                  <h3 className="design-item__title">{item.title}</h3>
+                  <p className="design-item__role">{item.role}</p>
+                  {item.images.map((image, index) => (
+                     <img key={index} className="design-item__image" src={image} alt={item.title} />
+                  ))}
+                  <p className="design-item__description">{item.description}</p>
+                  {item.link && <a className="design-item__link" target="_blank" rel="noreferrer" href={item.link.linkUrl}>{item.link.linkText}</a>}
+               </div>
+            ))}
+         </section>
 
-         <h3>Design Examples</h3>
-         <a className="site-link" href="https://helping-neighbors.nicholaskunz.com">Personal Web Develpment Project</a>
-         <a className="site-link" href="https://www.edgecitywoodworking.com/">EdgeCity Woodworking School (Photography and Layout)</a>
-         <a className="site-link" href="https://www.youtube.com/watch?v=9jEGLPJ2JR4">Video for BloodWizard x Adidad colab shoe (Illustrator layout and video production)</a>
-         <a
-            className="site-link"
-            href='/SNOW 13_14 FINAL DRAFT.pdf'
-            download='SNOW 13_14 FINAL DRAFT.pdf'>
-            Download iNi clothing catalog
-         </a>
-         <img className="dark-image" src="./nwlogofull.png" alt="NeedleWorks logo" />
-         <img className="dark-image" src="./fsbanner.png" alt="Forest Service Web Banner" />
 
-         <p>Well, I'll need to get my external harddrive out of storage (don't ask) to add more examples. I have to run today but if you don't check this until Monday, there will be more examples then :)</p>
+
          <img className="resume-image" src="/nicholas_and_pup.jpg" alt="Nicholas and Pup" />
          <p>We hope you have a wonderful day!</p>
+         <p>Nicholas (and Cappo) Kunz</p>
+         <p>MrNicholasKunz@gmail.com</p>
 
       </main>
 
